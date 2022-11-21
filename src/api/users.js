fe7f3569d424f0f2df37fe52905a5dfb6536fe57
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-11-21 14:23:30 -0600

    commented out possible users api

--- src/api/users.js
+++ src/api/users.js
@@ -1,1 +1,39 @@
-const express = require("express")
+const express = require("express");
+
+// router.post("/login" async (req,res,next) => {
+//     const {username, password } = req.body
+//     if (!username || !password) {
+//         next ({
+//             name:"Missing Credentials Error",
+//             message:"Please give both a username and password"
+
+//         });
+//     } else {
+//         try {
+//             const user = await getUserInfo({username, password})
+
+//             if (user) {
+//                 const token = jwt.sign(
+//                     {
+//                         id: user.id,
+//                         username,
+//                       },
+//                       JWT_SECRET,
+//                       {
+//                         expiresIn: "1w",
+//                       }
+//                 )
+//                 res.send({ user, token, message: "you're logged in!" });
+//             } else {
+//                 next ({
+//                     name:"Incorrect Credentials Error",
+//                      message:"Please give both a username and password"
+//                 })
+//             }
+
+//         } catch (error) {
+//             console.log(error);
+//             next(error);
+//           }
+//     }
+// })
