1dd1be94d3b59d3e0f35ddf86026b18a7666bfce
minimyl <aandrew.myles@gmail.com>
2022-11-21 14:15:02 -0500

    server setUp

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const { PORT = 8080 } = process.env;
+const express = require("express");
+const server = express();
+const morgan = require("morgan");
+server.use(morgan("dev"));
+const cors = require("cors");
+server.use(cors());
+
+server.use(express.json());
+const { client } = require("./db");
+client.connect();
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
+server.use((req, res, next) => {
+  next();
+});
+
+server.listen(PORT, () => {
+  console.log("The server is up on", PORT);
+});
