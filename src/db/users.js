8b0bfcbf25933832ab244b076eb9154503b0fc1d
minimyl <aandrew.myles@gmail.com>
2022-11-21 15:22:16 -0500

    users skeleton

---
+++ src/db/users.js
@@ -0,0 +1,16 @@
+const client = require("./index");
+
+async function createUser() {}
+
+async function getUser() {}
+
+async function getUserById() {}
+
+async function getUserByUserName() {}
+
+module.exports = {
+  createUser,
+  getUser,
+  getUserById,
+  getUserByUserName,
+};
