586ebdd9df900b38cb502814b81e12d5eeb468e9
timothy <timothybrennan8@gmail.com>
2022-11-22 11:47:20 -0500

    removed the seed file and moved it into seedData and renamed it seed for clarity

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,6 +1,61 @@
-const client = require('./client');
-const { rebuildDB } = require('./seedData');
+const {createUser} = require('.')
+const client = require('./client')
 
-rebuildDB()
+async function dropTables(){
+  try {
+    console.log("Dropping all tables...")
+    await client.query(`
+    DROP TABLE IF EXISTS products; 
+    DROP TABLE IF EXISTS users;
+    `)
+    
+    console.log("Finished dropping all tables...")  
+  } catch (error) {
+      console.error("Error dropping tables!")
+      throw error;
+  }  
+  
+}
+
+async function createTables(){
+  try {
+    console.log("Creating tables...")
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL, 
+      description TEXT NOT NULL
+    );
+    CREATE TABLE products (
+      
+    )
+    `)
+    console.log("Finished creating tables...")
+  } catch (error) {
+      console.error("Error building tables!")
+      throw error;
+  }
+    
+}
+
+
+async function rebuildDB() {
+    try {
+      await dropTables()
+      await createTables()
+    //   await createInitialUsers()
+    //   await createInitialActivities()
+    } catch (error) {
+      console.log("Error during rebuildDB")
+      throw error
+    }
+  }
+
+  async function testDB() {
+    
+  }
+  
+  rebuildDB()
+  .then(testDB)
   .catch(console.error)
   .finally(() => client.end());
